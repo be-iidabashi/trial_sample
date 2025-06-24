eebecd0479787e16b69ae37ea50c9426b5118504
katsukikuroda <katsukikuroda@gmail.com>
2025-06-24 17:41:23 +0900

    1stcmt

---
+++ ex1_files/main.js
@@ -0,0 +1,7 @@
+window.addEventListener("DOMContentLoaded", function () {
+  const alertBtn = document.getElementById("alert-btn");
+  alertBtn.addEventListener("click", function (e) {
+    window.alert("This is the cheese introduction page");
+    
+  });
+});
---
+++ ex3_files/main.js
@@ -0,0 +1,6 @@
+window.addEventListener("DOMContentLoaded", function () {
+  const alertBtn = document.getElementById("alert-btn");
+  alertBtn.addEventListener("click", function (e) {
+    window.alert("お買い求めはは左端の数研出版のアイコンから");
+  });
+});
